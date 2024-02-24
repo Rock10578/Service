f35354dd2069d9304ae890fcf6013c7c5ab17194
rock10578 <shreyanshpatwa26@gmail.com>
2024-02-24 13:15:54 +0530

    Changes to ignore .env

--- server/index.js
+++ server/index.js
@@ -2,16 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
-const connectDB = require("./utiils/db")
 
 // MiddleWare
 app.use(express.json())
 
 app.use("/api/auth", router);
 
-connectDB().then(() => {
-    const PORT = 4000
-    app.listen(PORT,() => {
-        console.log(`Server running on http://localhost:${PORT}/api/auth`)
-    })
+const PORT = 4000
+app.listen(PORT,() => {
+    console.log(`Server running on http://localhost:${PORT}/api/auth`)
 })
