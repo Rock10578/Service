31d3f2b8745be53e2f810908e0bd44730b9bdd31
rock10578 <shreyanshpatwa26@gmail.com>
2024-02-24 13:12:51 +0530

    Update .gitignore

--- server/utiils/db.js
+++ server/utiils/db.js
@@ -1,10 +1,8 @@
-const mongoose = require("mongoose")
-
-const MONGO_URL = "mongodb+srv://Service:Service1234@cluster0.pcs96bn.mongodb.net/User"
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("Connection to DB Successful")
     } catch (error) {
         console.log("Connection to DB Failed")
