aad641207d55313e734cfca5b88d760f28c31edf
rock10578 <shreyanshpatwa26@gmail.com>
2024-02-24 13:56:11 +0530

    data pushed to mongoDB

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user-model")
+
 const home = async (req,res) => {
     try {
         res
@@ -10,10 +12,13 @@ const home = async (req,res) => {
 
 const register = async (req,res) => {
     try {
-        console.log(req.body)
-        res
-            .status(200)
-            .json({msg: req.body})
+        const {username, password, email, phone} = req.body;
+        const userExist = await User.findOne({email: email})
+        if (userExist) {
+            return res.status(400).json({msg: "Email already exists"})
+        }
+        const userCreated = await User.create({username, password, email, phone})
+        res.status(200).json({msg: userCreated})
     } catch (error) {
         res.status(400).send({msg: "Page not found"})
     }
