3b91b6a22a95d92c8e1f5f07afe9bb1cfd4a8c1a
rock10578 <shreyanshpatwa26@gmail.com>
2024-03-05 19:05:06 +0530

    Registraion page frontend code

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,3 +1,73 @@
 export const Register = () => {
-    return <h1>Hello Register Page</h1>
+    return <>
+        <section>
+            <main>
+                <div className="section-registration">
+                    <div className="container grid grid-two-cols">
+                        <div className="registration-image">
+                            <img
+                                src="/images/register.png" 
+                                alt="A girl registring herself"
+                                width="500"
+                                height="500"
+                            />
+                        </div>
+                        <div className="registration-form">
+                            <h1 className="main-heading mb-3">Registration Form</h1>
+                            <br/>
+
+                            <form>
+                                <div>
+                                    <label htmlFor="username">username</label>
+                                    <input 
+                                        type="text" 
+                                        name="username" 
+                                        placeholder="username" 
+                                        id="username"
+                                        required
+                                        autoComplete="off"
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="password">password</label>
+                                    <input 
+                                        type="password" 
+                                        name="password" 
+                                        placeholder="password" 
+                                        id="password"
+                                        required
+                                        autoComplete="off"
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="email">email</label>
+                                    <input 
+                                        type="text" 
+                                        name="email" 
+                                        placeholder="Enter Your email" 
+                                        id="email"
+                                        required
+                                        autoComplete="off"
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="phone">phone</label>
+                                    <input 
+                                        type="number" 
+                                        name="phone" 
+                                        placeholder="Enter your phone no" 
+                                        id="phone"
+                                        required
+                                        autoComplete="off"
+                                    />
+                                </div>
+                                <br/>
+                                <button type="submit" className="btn btn-submit">Register Now</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </section>
+    </>
 }
