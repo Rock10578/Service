e13a7d5ef9be52bacab9f02a4b101e64e710bceb
rock10578 <shreyanshpatwa26@gmail.com>
2024-03-07 02:48:10 +0530

    Footer Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import {Service} from "./pages/Service"
 import {Register} from "./pages/Register"
 import {Login} from "./pages/Login"
 import {Navbar} from "./components/Navbar";
+import { Footer } from "./components/Footer";
 // import "./index.css"
 
 
@@ -21,6 +22,7 @@ const App = () => {
             <Route path="/register" element={<Register/>}/>
             <Route path="/login" element={<Login/>}/>
         </Routes>
+        <Footer/>
       </BrowserRouter>
   );
 };
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+export const Footer = () => {
+    return (
+        <>
+            <div className="footer">
+                <h2>&copy; Rock10578</h2>
+            </div>
+        </>
+    )
+}
