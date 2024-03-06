957bde7e4e4ca21234e955e62029a7a3cc0c28b2
rock10578 <shreyanshpatwa26@gmail.com>
2024-03-07 02:28:01 +0530

    Home page created

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,35 @@
 export const About = () => {
-    return <h1>Hello About Page</h1>
+    return(
+        <>
+            <main>
+            <section className="section-hero">
+                <div className="container grid grid-two-cols">
+                    <div className="hero-content">
+                        <p>Welcome, Rock the Developer web</p>
+                        <h1>Why Choose Us?</h1>
+                        <p>
+                            Expertise: Our team 
+                        </p>
+                        <div className="btn btn-group">
+                            <a href="/contact">
+                                <button className="btn">Connect Now</button>
+                            </a>
+                            <a href="/service">
+                                <button className="btn secondary-btn">Learn More</button>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="her-image">
+                        <img 
+                            src="/images/home.png"
+                            alt="coding together"
+                            width="400"
+                            height="500"
+                        />
+                    </div>
+                </div>
+            </section>
+        </main>
+        </>
+    )
 }
