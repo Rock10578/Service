d5815e0ecc911fb31c7c40b8410c647f6d7e56fa
rock10578 <shreyanshpatwa26@gmail.com>
2024-03-10 15:25:21 +0530

    contact fetching in backend done

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -1,3 +1,4 @@
+const Contact = require("../models/contact-model");
 const  User = require("../models/user-model");
 
 const getAllUsers = async (req,res) => {
@@ -9,8 +10,20 @@ const getAllUsers = async (req,res) => {
         }
         res.status(200).json(users);
     } catch (error) {
-        res.status("message : ",error);
+        res.status(400).json({message: error});
     }
 };
 
-module.exports = getAllUsers
+const getAllContacts = async (req,res) => {
+    try {
+        const contacts = await Contact.find();
+        if (!contacts || contacts.length === 0){
+            return res.status(404).json({message: "No user Found"});
+        }
+        return res.status(200).json(contacts)
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+}
+
+module.exports = { getAllUsers, getAllContacts }
--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const getAllUsers = require("../controllers/admin-controller");
+const { getAllUsers, getAllContacts } = require("../controllers/admin-controller");
 const router = express.Router();
 
 router.route('/users').get(getAllUsers);
+router.route('/contacts').get(getAllContacts);
 
 module.exports = router;
